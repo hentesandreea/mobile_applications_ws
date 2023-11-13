991cb763bed76137dc443ca1ff58411a0eef4d9b
hentesandreea <deeahe@gmail.com>
2023-11-13 20:31:36 +0200

    4th Workshop

--- ws4/appWs/DownloadComponent.js
+++ ws4/appWs/DownloadComponent.js
@@ -1,16 +1,75 @@
-import {View, StyleSheet, Image} from "react-native";
+import {View, StyleSheet, Image, Text} from "react-native";
 
 export default function DownloadComponent(){
     return(
         <View style ={styles.container}>
-        </View>
+            <View style ={styles.card}>
+                <View style = {styles.text}>
+                    <Text style={styles.name}>Resume - Mihai Pop</Text>
+                    <Text style ={styles.subtext}>PDF File</Text>
+                    <Text style ={styles.kbt}>23.59Kb</Text>
+                </View>
+                <View style ={styles.icon}>
+                        <Image source={{uri:'https://cdn-icons-png.flaticon.com/512/189/189249.png'}}
+                               style={styles.image}/>
+            </View>
+
+                </View>
+            </View>
+
     )
 }
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        backgroundColor: '#F1F0E8',
+        backgroundColor: 'gray',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    card:{
+        alignItems:'center',
+        width:350,
+        height:104,
+        borderRadius:14,
+        backgroundColor:'white',
+        shadowOpacity:0.3,
+
+    },
+    text:{
+        alignItems:'flex-start',
+    },
+    name:{
+        marginTop:16,
+        marginRight:140,
+        fontWeight:'bold',
+        fontSize:16.5,
+
+    },
+    subtext:{
+        color:'#54B435',
+        fontSize:12,
+    },
+    kbt:{
+        fontSize: 14,
+        marginTop:18,
+    },
+    icon:{
+        alignItems:'center',
+        marginEnd:26,
+        marginLeft:260,
+        width:80,
+        height:82,
+    },
+    image:{
+     alignItems:'center',
+        width:60,
+        height:60,
+        marginTop: -68,
+        position:'absolute',
+        right:2,
+
+
+
+    },
+
 })
