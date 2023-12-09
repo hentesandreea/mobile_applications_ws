b009a38d152888a953ec2ceaddd42ff1c46c5d11
hentesandreea <deeahe@gmail.com>
2023-12-09 20:04:16 +0200

    Components

--- ws4/appWs/KAlbum.js
+++ ws4/appWs/KAlbum.js
@@ -1,26 +1,54 @@
-import {View,Image,Text} from "react-native";
+import {View, Image, Text, TouchableOpacity} from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import KSpace from "./KSpace";
+
+
 
 const KAlbum = () => {
     return(
-        <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
-            <View style={{borderRadius:10, backgroundColor:'#FF842B', padding:20,shadowOpacity:0.2,height:'40%',width:'60%', gap:10}}>
-                <View style={{flexDirection:'row',gap:30}}>
-                <Image source={{uri:'https://images.unsplash.com/photo-1553378950-0ba726c8ca1b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
-                       style={{height:100,width:124,borderRadius:20}}
-                />
-                <Image source ={{uri:'https://cdn.icon-icons.com/icons2/3252/PNG/512/share_ios_regular_icon_204964.png'}}
-                       style={{height:30, width:30}}
-                />
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            {/*<LinearGradient
+                colors={['#FF842B', '#fc3d03']}
+                start={{x: 0, y: 0}}
+                end={{x: 1, y: 1}}
+                style={{flex: 1}}>*/}
+                <View style={{borderRadius: 20,backgroundColor:'#FF842B' ,padding: 20, shadowOpacity: 0.2, height: '30%', width: '90%', gap: 10}}>
+                    <View style={{flexDirection: 'row', gap: 16}}>
+                        <Image
+                            source={{uri: 'https://images.unsplash.com/photo-1553378950-0ba726c8ca1b?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}}
+                            style={{height: 160, width: 160, borderRadius: 20}}
+                        />
+                        <View style={{flexDirection: 'column', gap: 3,justifyContent:'center'}}>
+                            <KSpace h={6}/>
+                            <Text style={{
+                                fontSize: 14,
+                                fontWeight: '700',
+                                color: 'black',
+                                textTransform: 'uppercase',
+                                letterSpacing: 0.6
+                            }}>Nowhere you</Text>
+                            <Text style={{fontSize: 18, fontWeight: '600', letterSpacing: 0.6}}>Joyce Lynn</Text>
+                            <Text style={{
+                                fontSize: 15,
+                                fontWeight: 'bold',
+                                color: 'white',
+                                textTransform: 'uppercase',
+                                letterSpacing: 0.6
+                            }}>album</Text>
+                            <Text style={{fontSize: 15, fontWeight: 'bold', color: 'white', letterSpacing: 0.6}}>Pop ~
+                                2021</Text>
+                            <KSpace h={3}/>
+                            <TouchableOpacity onPress={() => alert('Play album')}
+                                style={{alignItems:'center'}}>
+                                <Image source={{uri:'https://static.thenounproject.com/png/117815-200.png'}}
+                                       style={{width:48,height:48}}
+                                />
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>
                 </View>
-                <View style={{flexDirection:'column', gap:2}}>
-                    <Text style={{fontSize:14}}>Joyce Lynn</Text>
-                    <Text style={{fontSize:14, fontWeight:'600'}}>Nostalgy of us</Text>
-                </View>
-                    <Text style={{fontSize:15, fontWeight:'bold',color:'white',textShadowOffset:0.5}}>Nowhere, everywhere</Text>
-
-
-            </View>
-
+            {/*</LinearGradient>*/}
         </View>
     )
 }
---
+++ ws4/appWs/KSpace.js
@@ -0,0 +1,9 @@
+import{View} from "react-native";
+
+function KSpace ({h = 10}){
+    return(
+        <View style={{height: h}}>
+        </View>
+    )
+}
+export default KSpace;
